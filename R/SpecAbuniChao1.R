d758a8c003024924dbaacb38fbf4496c474dca33
AnneChao <ljchao2012@gmail.com>
2015-12-28 10:36:04 +0800

    correct ichao1

--- R/SpecAbuniChao1.R
+++ R/SpecAbuniChao1.R
@@ -118,7 +118,7 @@ SpecAbuniChao1 <-
     xx <- 1:4
     i <- rep(sort(unique(xx)),each = length(unique(xx)))
     j <- rep(sort(unique(xx)),length(unique(xx)))       # all combination
-    ##f4 <- ifelse(f4>0,f4,1)
+    f4 <- ifelse(f4>0,f4,1)
     if (f1 - f2*f3/2/f4 > 0 & f3 != 0){
       var_iChao1 <- sum(mapply(function(i, j)diff(i, x)*diff(j, x)*COV.f(i, j), i, j))
     } else {
